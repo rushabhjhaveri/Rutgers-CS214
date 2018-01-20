2894d39a69280e085c22111f168b2b8e74796e4f
Rushabh Jhaveri <rjhaveri98@gmail.com>
2018-01-20 12:56:22 -0500

    Alignments.

--- Asst0/stringsorter.c
+++ Asst0/stringsorter.c
@@ -1,5 +1,18 @@
-//partner name: Rushabh Jhaveri
-//partner netID: rrj28
+/*
+======================================
+Class: Systems Programming [CS214]
+
+Semester: Spring 2018
+
+Assignment 0
+
+Collaborator Name       NetID
+
+Hetalben Patel          hp373
+
+Rushabh Jhaveri         rrj28
+=========================================
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,50 +21,52 @@
 
 #include "stringsorter.h"
 
-int debug = 1;
+unsigned int DEBUG = 1; // NO DEBUG = 0 ; DEBUG = 1
 
 int main(int argc, char *argv[]) {
+	/*
+	   types of error:
+           1. null string
+           2. no input
+           3. more than 1 input
+           4. no alphabets
+         */
 
-  /*
-  types of error:
-  1. null string
-  2. no input
-  3. more than 1 input
-  4. no alphabets
-  */
+	//Variable declarations.
 
-  if(argc != 2)
-  {
-    fprintf(stderr, "%s\n", "ERROR: Invalid number of arguments");
-    exit(0);
-  }
 
-  char * string = argv[1];
-  printf("String: %s\n", string);
+  	if(argc != 2)
+	{
+		fprintf(stderr, "%s\n", "ERROR: Invalid number of arguments");
+    		exit(0);
+  	}
 
-  int isChar = 0;
-  int notChar = 0;
-  int i;
+  	char * string = argv[1];
+  	printf("String: %s\n", string);
 
-  for(i = 0; i < strlen(string); i++)
-  {
-    if((string[i] >= 'a' && string[i] <= 'z') || (string[i] >= 'A' && string[i] <= 'Z'))
-    {
-      isChar++;
-    }
-    else
-    {
-      notChar++;
-    }
-  }
+  	int isChar = 0;
+  	int notChar = 0;
+  	int i;
 
-  printf("isChar count: %d\n", isChar);
-  printf("notChar count: %d\n", notChar);
+  	for(i = 0; i < strlen(string); i++)
+  	{
+		if((string[i] >= 'a' && string[i] <= 'z') || (string[i] >= 'A' && string[i] <= 'Z'))
+		{
+			isChar++;
+    		}
+    		else
+    		{
+      			notChar++;
+    		}
+  	}
 
-  if(isChar == 0)
-  {
-    fprintf(stderr, "%s\n", "No alphabets");
-    exit(0);
-  }
-  return 0;
+  	printf("isChar count: %d\n", isChar);
+  	printf("notChar count: %d\n", notChar);
+
+  	if(isChar == 0)
+  	{
+    		fprintf(stderr, "%s\n", "No alphabets");
+    		exit(0);
+  	}
+  	return 0;
 }
