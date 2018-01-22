fd26d66f823d206c4a8960a08fb7a7c0e6c83c52
Rushabh Jhaveri <rjhaveri98@gmail.com>
2018-01-21 21:08:03 -0500

    Updated trim method to take care of all separators.

--- Asst0/stringsorter.c
+++ Asst0/stringsorter.c
@@ -29,14 +29,16 @@ char *trimwhitespace(char *str)
 	  char *end;
 
 	    // Trim leading space
-	    while(isspace((unsigned char)*str)) str++;
+	    //while(isspace((unsigned char)*str)) str++;
+	    while(!isalpha((unsigned char)*str)) str++;
 
 	      if(*str == 0)  // All spaces?
 		          return str;
 
 	        // Trim trailing space
 	        end = str + strlen(str) - 1;
-		  while(end > str && isspace((unsigned char)*end)) end--;
+		  //while(end > str && isspace((unsigned char)*end)) end--;
+		  while(end > str && (!isalpha((unsigned char)*end))) end--;
 
 		    // Write new null terminator
 		    *(end+1) ='\0';
